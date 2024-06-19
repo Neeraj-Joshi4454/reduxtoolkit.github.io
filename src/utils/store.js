37e305129de043e79a9919c4f223bf23804f5ce8
Neeraj-Joshi4454 <neerajmjoshi1@gmail.com>
2024-06-19 20:00:21 +0530

    intiial comit

---
+++ src/utils/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
+import userSlice from "./userSlice";
+const store = configureStore({
+    reducer : {
+        cart : cartSlice,
+        users : userSlice,
+    }
+})
+
+export default store;
